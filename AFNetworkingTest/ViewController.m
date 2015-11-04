7092d33762d7720137056d0d73cc1a081aa6acdd
Ethan Jud <ethanpjud@gmail.com>
2015-11-04 15:09:46 -0800

    Scenario for AFNetworking race condition

--- AFNetworkingTest/ViewController.m
+++ AFNetworkingTest/ViewController.m
@@ -7,12 +7,48 @@
 //
 
 #import "ViewController.h"
+#import <AFNetworking/AFNetworking.h>
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+
+@property (strong, nonatomic) AFHTTPSessionManager *sessionManager;
 
 @end
 
 @implementation ViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        [self setupSessionManager];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self setupSessionManager];
+    }
+    return self;
+}
+
+- (void)setupSessionManager
+{
+    self.sessionManager = [AFHTTPSessionManager manager];
+
+    __weak typeof(self) weakSelf = self;
+    [self.sessionManager.reachabilityManager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        NSLog(@"Reachability status changed: %@", AFStringFromNetworkReachabilityStatus(status));
+
+        weakSelf.textView.text = [weakSelf.textView.text stringByAppendingFormat:@"Reachability status changed: %@\n", AFStringFromNetworkReachabilityStatus(status)];
+    }];
+    [self.sessionManager.reachabilityManager startMonitoring];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
