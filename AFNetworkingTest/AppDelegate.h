048deaffcc02e811c4570aef0a4d6fb84ad9360a
Ethan Jud <ethanpjud@gmail.com>
2015-11-04 14:54:00 -0800

    Initial Commit

---
+++ AFNetworkingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkingTest
+//
+//  Created by Ethan Jud on 11/4/15.
+//  Copyright © 2015 8tracks, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingTest
+//
+//  Created by Ethan Jud on 11/4/15.
+//  Copyright © 2015 8tracks, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
